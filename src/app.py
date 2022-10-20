4eaca4f406364aef02fa3569e3132fb3d5604d3e
teroufi <anass.teroufi@gmail.com>
2022-10-20 13:48:12 +0000

    flaskwb

---
+++ src/app.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template, json
+from flaskext.mysql import MySQL
+
+mysql = MySQL()
+app = Flask(__name__)
+app.config.from_pyfile('database.cfg')
+
+mysql.init_app(app)
+
+@app.route('/')
+def main():
+    version = showVersion()
+    return render_template('index.html', version=version)
+
+def showVersion():
+    try:
+        myfile = 'tmp/app-version.txt'
+        with open(myfile) as f:
+            message = '{0}'.format(f.read())
+    except IOError:
+        message = '\n\nFile not found: ' + myfile
+    return message
+
+
+@app.route('/students')
+def getStudents():
+    version = showVersion()
+    try:
+        # connect to mysql
+        connection = mysql.connect()
+        cursor = connection.cursor()
+        cursor.execute('''SELECT * FROM students''')
+        result = cursor.fetchall()
+	    
+        students = []
+        for entry in result:
+            record = {
+                    'id': entry[0],
+                    'name': entry[1],
+            }
+            students.append(record)		
+
+        return render_template('db.html', version=version, students=students)
+    except Exception as e:
+        return json.dumps({'error':str(e)})
+
+    except Exception as e:
+        return json.dumps({'error':str(e)})
+    finally:
+        cursor.close()
+        connection.close()
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
